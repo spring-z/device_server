ad73261f01e798c1704c701eca398dc439e45329
zcy19930125 <zcy19930125@163.com>
2016-10-28 23:08:24 +0800

    update libepoll

--- public/LibEpoll.c
+++ public/LibEpoll.c
@@ -5,24 +5,83 @@
 #include "Libloger.h"
 
 
-
-
-
-
-
-int CreatEpollSet(int maxEvents)
+EpollSet_t* CreatEpollSet(int maxEvents)
 {
-	int epollFd;
-	
-	epollFd = epoll_create(maxEvents);
-	if(epollFd < 0)
+	EpollSet_t* epollSet = (EpollSet_t*)malloc(sizeof(EpollSet_t));
+	if(epollSet != NULL)
 	{
-		error("epoll creat error:");
-		return -1;
+		epollSet->events = (struct epoll_event*)malloc(sizeof(struct epoll_event));
+		if(epollSet->events == NULL)
+		{
+			error("epoll events malloc error:");
+			free(epollSet)
+			epollSet = NULL;
+			return NULL;
+		}
+		
+		epollSet->epoll_fd = epoll_create(maxEvents);
+		if(epollSet->epoll_fd < 0)
+		{
+			error("epoll creat error:");
+			free(epollSet)
+			epollSet = NULL;
+			return NULL;
+		}
+		
+		epollSet->max_events = maxEvents;
 	}
 	
 	
+	return epollSet;
 }
+
+
+int EpollSetAddFd(EpollSet_t* epollSet, int fd)
+{
+	struct epoll_event ev;
+	
+	ev.data.fd = fd;	
+	ev.events = EPOLLIN;	//epoll watch data in
+	if(epoll_ctl(epollSet->epoll_fd, EPOLL_CTL_ADD, fd, &ev) < 0)
+	{
+		error("epoll_ctl add error:");
+		return -1;
+	}
+	
+	return 1;
+}
+
+
+int EpollSetDeleteFd(EpollSet_t* epollSet, int fd)
+{
+	if(epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL) < 0)
+	{
+		error("epoll_ctl del error:");
+		return -1;
+	}
+	
+	close(fd);
+	return 1;
+}
+
+
+
+
+int EpollSetWait(EpollSet_t* epollSet)
+{
+	return epoll_wait(epollSet->epoll_fd, epollSet->events, epollSet->max_events, -1);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- public/LibEpoll.h
+++ public/LibEpoll.h
@@ -2,9 +2,16 @@
 #define _LIBEPOLL_H_
 
 
+#typedef struct {
+	int epoll_fd;
+	int max_events;
+	struct epoll_event* events;
+} EpollSet_t;
 
-
-
+EpollSet_t* CreatEpollSet(int maxEvents);
+int EpollSetAddFd(EpollSet_t* epollSet, int fd);
+int EpollSetDeleteFd(EpollSet_t* epollSet, int fd);
+int EpollSetWait(EpollSet_t* epollSet);
 
 
 
