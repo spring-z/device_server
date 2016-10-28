1a57714c4d01139501858885c5997164d961a171
zcy19930125 <zcy19930125@163.com>
2016-10-28 18:17:27 +0800

    upadate libepoll

--- public/LibEpoll.c
+++ public/LibEpoll.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/epoll.h>
 #include "LibEpoll.h"
+#include "Libloger.h"
 
 
 
@@ -9,7 +10,19 @@
 
 
 
-
+int CreatEpollSet(int maxEvents)
+{
+	int epollFd;
+	
+	epollFd = epoll_create(maxEvents);
+	if(epollFd < 0)
+	{
+		error("epoll creat error:");
+		return -1;
+	}
+	
+	
+}
 
 
 
