2959fe3d2b0e2cac804b65ecd0b3c31c2a071a5e
zcy19930125 <zcy19930125@163.com>
2016-10-31 21:08:51 +0800

    update folder/msgqueue

--- public/LibMsgQueue.c
+++ public/LibMsgQueue.c
@@ -1,4 +1,61 @@
 #include <stdio.h>
+#include "LibLoger.h"
 
+/*
+	不同进程使用同一个key创建消息队列；
+ msg exp:
+	struct msg_st {
+	  long int msg_type;
+	  char text[BUFSIZE];
+	};
+ */ 
+
+
+int CreatMessageQueue(key_t msgKey)
+{
+	int msgid = -1;
+	msgid = msgget(msgKey, 0666 | IPC_CREAT);
+	if(msgid == -1)
+	{
+		error("msg queue creat error:");
+	}
+	return msg;
+}
+
+
+int MsgQueueSend(int msgid, void* msg, size_t msgLen)
+{
+	if (msgsnd(msgid, msg, msgLen, 0) == -1) 
+	{
+		error("msg send error:");
+		return -1;
+	}
+	return 1;
+}
+
+/* 
+ * msgType : if 0: 顺序接收
+ * msgLen : 接收数据长度，不包含长整型msgType长度
+ *
+ */
+int MsgQueueRecv(int msgid, void* msg, size_t msgLen, long int msgType)
+{
+	if (msgrcv(msgid, (void *)&msg, msgLen, msgType, 0) == -1) 
+    {
+      error("msg queue recv error:");
+      return -1;
+    }
+	return 1;
+}
+
+int MsgQueueDelete(int msgid)
+{
+  if (msgctl(msgid, IPC_RMID, 0) == -1) 
+  {
+    error("msg queue delete error:");");
+    return -1;
+  }
+  return 1;
+}
 
 
--- public/LibMsgQueue.h
+++ public/LibMsgQueue.h
@@ -2,15 +2,10 @@
 #define _LIBMSGQUEUE_H_
 
 
-
-
-
-
-
-
-
-
-
+int CreatMessageQueue(key_t msgKey);
+int MsgQueueSend(int msgid, void* msg, size_t msgLen);
+int MsgQueueRecv(int msgid, void* msg, size_t msgLen, long int msgType);
+int MsgQueueDelete(int msgid);
 
 
 
