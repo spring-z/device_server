549f09ab7fc9887918044b9a4f888b1bd9d83070
zcy19930125 <zcy19930125@163.com>
2016-11-04 15:17:36 +0800

    update test

---
+++ test/uni_epoll.c
@@ -0,0 +1,68 @@
+#include "stdio.h"
+#include "LibSocket.h"
+#include "LibEpoll.h"
+
+#define MAX_BUFF_LEN 1024
+int buff[MAX_BUFF_LEN];
+
+
+int main(void)
+{
+	int listenFd;
+	EpollSet_t* epollSet;
+	int fds;
+	
+	if((listenFd = StartupSocket(7222)) == -1)
+		return -1;
+	if((epollSet = CreatEpollSet(500) == NULL)
+		return -1;
+	if(EpollSetAddFd(epollSet,listenFd) == -1)
+		return -1;
+	
+	while(1)
+	{
+		fds = EpollSetWait(epollSet));
+		if(fds < 0)
+		{
+			perror("wait error:");
+			return -1;
+		}
+		for(i=0; i<fds; i++)
+		{
+			if(epollSet.events[i].data.fd == listenFd)
+			{
+				int sock_fd;
+				sock_fd = accept(listenFd, (struct sockaddr*)NULL, NULL);
+				if(sock_fd < 0)
+				{
+					perror("accept error:");
+					continue;
+				}
+			    printf("accept sock:%d\n",sock_fd);	
+				EpollSetAddFd(epollSet,sock_fd);
+				continue;
+			}
+			else
+			{		
+				int bytes;
+				bytes = recv(epollSet.events[i].data.fd, buff, MAX_BUFF_LEN, 0);
+				if(bytes <= 0)
+				{
+					EpollSetDeleteFd(epollSet, epollSet.events[i].data.fd);
+					close(epollSet.events[i].data.fd);
+					continue;
+				}
+				else
+				{
+					printf("%s\n",buff);
+				}
+			}
+			
+		}
+	}
+	
+	
+	
+}
+
+
