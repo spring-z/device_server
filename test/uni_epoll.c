35a4cb257d96744799bf05d88711a2a8ca6d70c0
zcy19930125 <zcy19930125@163.com>
2016-11-10 21:57:00 +0800

    更新目录结构

--- test/uni_epoll.c
+++ test/uni_epoll.c
@@ -1,27 +1,50 @@
 #include "stdio.h"
 #include "LibSocket.h"
 #include "LibEpoll.h"
+#include "LibLoger.h"
+#include "LibMsgQueue.h"
 
+#define MSGQUEUE_KEY 718
 #define MAX_BUFF_LEN 1024
-int buff[MAX_BUFF_LEN];
+//int buff[MAX_BUFF_LEN];
 
+
+struct msg_st {
+  long int msg_type;
+  char buff[MAX_BUFF_LEN];
+} msg_buff;
+
+
+int msgId;
 
 int main(void)
 {
 	int listenFd;
 	EpollSet_t* epollSet;
 	int fds;
+	int i;
 	
+	info("starting a listen 7222\n");
 	if((listenFd = StartupSocket(7222)) == -1)
 		return -1;
-	if((epollSet = CreatEpollSet(500) == NULL)
+	
+	info("listen : fd = %d\n", listenFd);
+	
+	if((epollSet = CreatEpollSet(500)) == NULL)
 		return -1;
+	
+	info("creat epoll ok: fd = %d\n", listenFd);
+	
 	if(EpollSetAddFd(epollSet,listenFd) == -1)
 		return -1;
 	
+	if((msgId = CreatMessageQueue(MSGQUEUE_KEY)) == -1)
+		return -1;
+	
+	
 	while(1)
 	{
-		fds = EpollSetWait(epollSet));
+		fds = EpollSetWait(epollSet);
 		if(fds < 0)
 		{
 			perror("wait error:");
@@ -29,7 +52,7 @@ int main(void)
 		}
 		for(i=0; i<fds; i++)
 		{
-			if(epollSet.events[i].data.fd == listenFd)
+			if(epollSet->events[i].data.fd == listenFd)
 			{
 				int sock_fd;
 				sock_fd = accept(listenFd, (struct sockaddr*)NULL, NULL);
@@ -45,16 +68,19 @@ int main(void)
 			else
 			{	
 				int bytes;
-				bytes = recv(epollSet.events[i].data.fd, buff, MAX_BUFF_LEN, 0);
+				bytes = recv(epollSet->events[i].data.fd, msg_buff.buff, MAX_BUFF_LEN, 0);
 				if(bytes <= 0)
 				{
-					EpollSetDeleteFd(epollSet, epollSet.events[i].data.fd);
-					close(epollSet.events[i].data.fd);
+					EpollSetDeleteFd(epollSet, epollSet->events[i].data.fd);
+					close(epollSet->events[i].data.fd);
 					continue;
 				}
 				else
 				{
-					printf("%s\n",buff);
+					printf("%s\n",(char*)msg_buff.buff);
+					msg_buff.msg_type = 1;
+					printf("send %s, type = %ld, len = %ld\n", msg_buff.buff,msg_buff.msg_type,sizeof(struct msg_st));
+					MsgQueueSend(msgId,&msg_buff,sizeof(struct msg_st));
 				}
 			}
 			
